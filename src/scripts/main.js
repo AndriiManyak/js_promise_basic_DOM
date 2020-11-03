082a8eb071c9f40f2df5e4f06c7dbe6ff7e67ef8
VoduDay <andriy.maniak@gmail.com>
2020-11-03 20:27:29 +0200

    promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function clickResolve(resolve) {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+}
+
+function delayResolve(_, reject) {
+  setTimeout(() => {
+    reject();
+  }, 5000);
+}
+
+const clickPromise = new Promise(clickResolve);
+const delayPromise = new Promise(delayResolve);
+
+// eslint-disable-next-line no-console
+clickPromise.then(() => console.log('resolved'));
+// eslint-disable-next-line no-console
+delayPromise.catch(() => console.log('rejected'));
